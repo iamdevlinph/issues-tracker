146c4503a210cdc206b51de14aac3cb75c0a5c04
Devlin Pajaron <iamdevlinph@gmail.com>
2026-04-24 19:10:58 +0800

    Switch from npm to pnpm

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import rsc from "@vitejs/plugin-rsc";
 import { defineConfig } from "vite";
 
 const config = defineConfig({
@@ -11,7 +12,12 @@ const config = defineConfig({
 		devtools(),
 		cloudflare({ viteEnvironment: { name: "ssr" } }),
 		tailwindcss(),
-		tanstackStart(),
+		tanstackStart({
+			rsc: {
+				enabled: true,
+			},
+		}),
+		rsc(),
 		viteReact(),
 	],
 });
