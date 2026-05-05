665b2ee3479a83e9ff1039b360d4208a9598aba2
Devlin Pajaron <iamdevlinph@gmail.com>
2026-05-05 19:04:57 +0800

    Remove rsc

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
-import rsc from "@vitejs/plugin-rsc";
 import { defineConfig } from "vite";
 
 const config = defineConfig({
@@ -11,16 +10,19 @@ const config = defineConfig({
 	plugins: [
 		devtools(),
 		cloudflare({
-			viteEnvironment: { name: "ssr", childEnvironments: ["rsc"] },
+			viteEnvironment: {
+				name: "ssr",
+				//  childEnvironments: ["rsc"]
+			},
 		}),
 		tailwindcss(),
 		tanstackStart({
-			rsc: {
-				enabled: true,
-			},
+			// rsc: {
+			// 	enabled: true,
+			// },
 		}),
 		// nitro(),
-		rsc(),
+		// rsc(),
 		viteReact(),
 	],
 });
