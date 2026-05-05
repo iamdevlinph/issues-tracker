97a3d784faea1156c9e0f902ae4c51eb9567faea
Devlin Pajaron <iamdevlinph@gmail.com>
2026-05-05 15:35:47 +0800

    Deploy to vercel

--- functions/_middleware.ts
+++
@@ -1,4 +0,0 @@
-export const onRequest: PagesFunction = async (context) => {
-  // Handle SSR requests
-  return await handleSSR(context)
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,25 @@
-import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import rsc from "@vitejs/plugin-rsc";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 
 const config = defineConfig({
 	resolve: { tsconfigPaths: true },
 	plugins: [
 		devtools(),
-		cloudflare({
-			viteEnvironment: { name: "ssr", childEnvironments: ["rsc"] },
-		}),
+		// cloudflare({
+		// 	viteEnvironment: { name: "ssr", childEnvironments: ["rsc"] },
+		// }),
 		tailwindcss(),
 		tanstackStart({
 			rsc: {
 				enabled: true,
 			},
 		}),
+		nitro(),
 		rsc(),
 		viteReact(),
 	],
