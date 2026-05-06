2245cd130bf0132ebe270501dc963ddf13d9c0fe
Devlin Pajaron <iamdevlinph@gmail.com>
2026-05-06 18:53:40 +0800

    Increase staleTime and gcTime

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
-const STALE_TIME_MINS = 2;
+const STALE_TIME_MINS = 5;
 export const STALE_TIME = STALE_TIME_MINS * 60 * 10000;
 
-const GC_TIME_MINS = 5;
+const GC_TIME_MINS = 10;
 export const GC_TIME = GC_TIME_MINS * 60 * 10000;
 
 export const REPOSITORIES_QUERY_KEY = "repositories";
